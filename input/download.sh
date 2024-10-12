46b86de883af3ad722bc72b14fa3468829f57af1
ElFazouani <106879507+ElFazouani@users.noreply.github.com>
2024-10-12 11:23:45 +0100

    Update download.sh

--- input/download.sh
+++ input/download.sh
@@ -6,6 +6,6 @@ unzip -q rsna-2024-lumbar-spine-degenerative-classification.zip -d .
 rm -r rsna-2024-lumbar-spine-degenerative-classification.zip
 
 echo 'download improved coordinates ...'
-kaggle datasets download -d brendanartley/lumbar-coordinate-pretraining-dataset -f coords_pretrain.csv
-unzip coords_pretrain.csv.zip -d .
-rm coords_pretrain.csv.zip
+kaggle datasets download -d brendanartley/lumbar-coordinate-pretraining-dataset -f coords_rsna_improved.csv
+unzip coords_rsna_improved.csv.zip -d .
+rm coords_rsna_improved.csv.zip
